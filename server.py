df916e77ae205fcd83641ae4379f23645befae2d
marwan-chehab <61636579+marwan-chehab@users.noreply.github.com>
2025-03-04 14:46:58 +0200

    Update server.py

--- server.py
+++ server.py
@@ -61,5 +61,38 @@ def submit_status():
         print(f"Unexpected error: {e}")
         return f"Unexpected error: {str(e)}", 500
 
+@app.route('/burner')
+def burner():
+    print("Getting burner...")
+
+    try:
+        connection = create_connection()
+        if not connection:
+            print("Failed to connect to the database")
+            return jsonify({"error": "Error connecting to database"}), 500
+        
+        try:
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM burner ORDER BY bur_id DESC LIMIT 1")
+            result = cursor.fetchone()
+            connection.close()
+
+            if result:
+                print(f"onoff: {result['onoff']}")  # Debugging
+                print("Returning result to browser:", result)
+                return jsonify(result)  # Make sure result is JSON serializable
+            else:
+                print("No data found")
+                return jsonify({"error": "No data found"}), 404
+
+        except Error as e:
+            print(f"Error while executing query: {e}")
+            return jsonify({"error": "Error executing query"}), 500
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return jsonify({"error": "An unexpected error occurred"}), 500
+        
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
