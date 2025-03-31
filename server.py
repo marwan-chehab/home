1a4f6129d528973435a3d68838264a03c4084c6c
marwan-chehab <61636579+marwan-chehab@users.noreply.github.com>
2025-03-31 21:55:46 +0300

    Update server.py - added all registers

--- server.py
+++ server.py
@@ -113,47 +113,51 @@ def device(device_type):
 def growatt():
     data = request.get_json()
 
-    # Fields expected in the database
-    db_fields = [
-        'inverter_id', 'battery_voltage', 'battery_soc', 'output_current',
-        'inverter_current', 'inverter_temp', 'fan_speed_1', 'fan_speed_2',
-        'pv_input_power',  # <-- Will be mapped from pv_input_power_high
-        'grid_voltage', 'line_frequency', 'output_voltage',
-        'output_frequency', 'ac_charge_current', 'solar_buck1_current',
-        'solar_buck2_current', 'total_solar_charge_current',
-        'pv1_voltage', 'pv2_voltage', 'bus_voltage', 'dc_output_voltage',
-        'load_percent', 'battery_port_voltage', 'battery_bus_voltage',
-        'battery_watt_charge', 'battery_watt_discharge',
-        'buck1_temp', 'buck2_temp', 'work_time_low', 'work_time_high',
-        'device_type_code', 'system_status', 'fault_bit', 'warning_bit'
-    ]
+    expected_fields = [
+		'system_status', 'pv1_voltage', 'pv2_voltage', 'pv1_charge_power', 'pv2_charge_power', 
+		'buck1_current', 'buck2_current', 'output_active_power', 'output_apparent_power', 
+		'ac_charge_power', 'ac_charge_apparent_power', 'battery_voltage', 'battery_soc', 'bus_voltage', 
+		'grid_voltage', 'line_frequency', 'output_voltage', 'output_frequency', 'dc_output_voltage', 
+		'inverter_temp', 'dcdc_temp', 'load_percent', 'battery_port_voltage', 'battery_bus_voltage', 
+		'work_time_total', 'buck1_temp', 'buck2_temp', 'output_current', 'inverter_current', 'ac_input_power', 
+		'ac_input_va', 'fault_bit', 'warning_bit', 'fault_value', 'warning_value', 'device_type_code', 
+		'check_step', 'production_line_mode', 'constant_power_ok_flag', 'pv1_energy_today', 'pv1_energy_total', 
+		'pv2_energy_today', 'pv2_energy_total', 'ac_charge_energy_today', 'ac_charge_energy_total', 
+		'bat_discharge_energy_today', 'bat_discharge_energy_total', 'ac_discharge_energy_today', 
+		'ac_discharge_energy_total', 'ac_charge_current', 'ac_discharge_watt', 'ac_discharge_va', 
+		'bat_discharge_watt', 'bat_discharge_va', 'battery_watt', 'bat_overcharge_flag', 'mppt_fan_speed', 
+		'inv_fan_speed', 'bms_status', 'bms_error', 'bms_warn_info', 'bms_soc', 'bms_battery_volt', 'bms_battery_curr', 
+		'bms_battery_temp', 'bms_max_curr', 'bms_constant_volt', 'bms_info', 'bms_pack_info', 'bms_using_cap', 
+		'bms_cell1_volt', 'bms2_cell16_volt', 'bms2_status', 'bms2_error', 'bms2_warn_info', 'bms2_soc', 
+		'bms2_battery_volt', 'bms2_battery_curr', 'bms2_battery_temp', 'bms2_max_curr', 'bms2_constant_volt', 
+		'bms2_info', 'bms2_pack_info', 'bms2_using_cap', 'bms2_cell1_volt', 'bms2_cell16_volt', 'solar1_status', 
+		'solar1_fault_code', 'solar1_warning_code', 'solar1_bat_voltage', 'solar1_pv1_voltage', 'solar1_pv2_voltage', 
+		'solar1_buck1_current', 'solar1_buck2_current', 'solar1_pv1_charge_power', 'solar1_pv2_charge_power', 
+		'solar1_hs1_temp', 'solar1_hs2_temp', 'solar1_epv1_today', 'solar1_epv2_today', 'solar1_epv1_total', 
+		'solar1_epv2_total', 'solar2_status', 'solar2_fault_code', 'solar2_warning_code', 'solar2_bat_voltage', 
+		'solar2_pv1_voltage', 'solar2_pv2_voltage', 'solar2_buck1_current', 'solar2_buck2_current', 
+		'solar2_pv1_charge_power', 'solar2_pv2_charge_power', 'solar2_hs1_temp', 'solar2_hs2_temp', 
+		'solar2_epv1_today', 'solar2_epv2_today', 'solar2_epv1_total', 'solar2_epv2_total', 
+		'solar_connect_ok_flag', 'solar_batvolt_consist_flag', 'solar_type_switch_state', 
+		'solar_mode_switch_state', 'solar_address_switch_state', 'bms_gauge_rm', 'bms_gauge_fcc', 
+		'bms_fw', 'bms_delta_volt', 'bms_cycle_count', 'bms_soh', 'bms_ic_current', 'bms_mcu_version', 
+		'bms_gauge_version', 'bms_fr_version', 'bms2_gauge_rm', 'bms2_gauge_fcc', 'bms2_fw', 
+		'bms2_delta_volt', 'bms2_cycle_count', 'bms2_soh', 'bms2_ic_current', 'bms2_mcu_version', 
+		'bms2_gauge_version', 'bms2_fr_version', 'inverter_id']
 
-    # Required fields coming from the ESP32 (includes pv_input_power_high)
-    required_input_fields = db_fields.copy()
-    required_input_fields.remove('pv_input_power')
-    required_input_fields.append('pv_input_power_high')
-
-    # Validate incoming payload
-    if not all(field in data for field in required_input_fields):
-        missing = [field for field in required_input_fields if field not in data]
+    missing = [field for field in expected_fields if field not in data]
+    if missing:
         return jsonify({"error": f"Missing fields: {', '.join(missing)}"}), 400
 
     connection = create_connection()
     if not connection:
         return jsonify({"error": "Database connection failed"}), 500
 
-    # SQL statement
-    sql = f"""
-    INSERT INTO growatt (
-        {', '.join(db_fields)}
-    ) VALUES ({', '.join(['%s'] * len(db_fields))})
-    """
+    placeholders = ", ".join(["%s"] * len(expected_fields))
+    fields_str = ", ".join(expected_fields)
 
-    # Build values tuple, substituting pv_input_power from pv_input_power_high
-    values = tuple(
-        data['pv_input_power_high'] if field == 'pv_input_power' else data[field]
-        for field in db_fields
-    )
+    sql = f"INSERT INTO growatt ({fields_str}) VALUES ({placeholders})"
+    values = tuple(data[field] for field in expected_fields)
 
     try:
         cursor = connection.cursor()
